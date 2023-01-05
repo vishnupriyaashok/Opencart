76dd2bd0bd6e73ae7bca20ed65dcb0413c7f09e0
vishnupriyaashok <103440875+vishnupriyaashok@users.noreply.github.com>
2023-01-05 11:04:24 -0600

    Update Demo.java

--- src/main/java/com/qa/OpenCart/pages/Demo.java
+++ src/main/java/com/qa/OpenCart/pages/Demo.java
@@ -3,7 +3,9 @@ package com.qa.OpenCart.pages;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Demo567");
+		System.out.println("Demo890");
+		System.out.println("Demo5123");
 
 	}
 
